fc6d9c0c50478056a93a872c1cf06f58c0260c57
Trayoh <134661364+25401@users.noreply.github.com>
2023-06-19 04:07:23 +0300

    Settings.js
    🍓All change Settings Place☘️

---
+++ Settings.js
@@ -0,0 +1,150 @@
+// ```H O M I X-B O T```  #v1
+
+// • # Made by @Trayoh [ GitHub.com/25401 ]
+
+// • # The script is Encrypted because users are not allowed to commit any changes without permission!
+
+// • # To buy non enc version of Anya-pika-MD-v2 please contact wa.me/918811074852 [ $5 - $8 ]
+
+
+const fs = require('fs')
+
+const chalk = require('chalk')
+
+require('./lib/system/config')
+
+global.zApiKey = [// if this API key get expired then please go to https://www.fxacb-api.my.id and get a new api key.
+
+       'zenzkey_998568986d'
+
+]
+
+global.lApiKey = [ // if this API key get expired then please go to https://api.lannn.me and get a new api key.
+
+       'uMSPCuLU',
+
+       'IOGaHu5S'
+
+]
+
+global.botname = "Homix-Bot" 
+
+global.author = "𝘛𝘳𝘢𝘺𝘰𝘩"  
+
+global.packname = "HOHOMIX-BOT"  
+
+global.myweb = "https://github.com/25401" // You can put any link 
+
+global.waterMark = "TranMix" // Your own watermark 
+
+global.footer = "© Queen Anya Bot"
+
+global.prefa = ['/'] // single prefix
+
+global.themeemoji = '🇰🇪'
+
+global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
+
+global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
+
+global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name // You can change it to you preferred Name  
+
+global.ownernumber = ['254706519089'] 
+
+global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
+
+global.ytname = "YT: Ytname"   // Yt chanel name //
+
+global.continent = "Africa" // your continent name 
+
+global.region = "Kenya" // your country name
+
+global.state = "Kiambu" // your state name
+
+global.district = "Thika" // your district name
+
+global.timezone = "Africa/Nairobi" // search on google if you don't know the timezone of your country //
+
+global.instagramId = "3.69_pika" // Your insta ID
+
+global.email = "entrayoh@gmail.com" // put your email here
+
+// Greeting messages
+
+global.greet1 = "Good morning ☀️" // after 3 AM
+
+global.greet2 = "Good afternoon 🏜️" // after 12 PM
+
+global.greet3 = "Good evening 🌆" // after 4 PM
+
+global.greet4 = "Good night 😴" // after 8:30 PM
+
+//------------ 🌟 Premium Users 🌟-------------\\
+
+global.premium = ['916900******','917355******']  // available soon
+
+global.sudo = ['916900******','91881107****']  // Co - Owners
+
+//--------------- Tip ----------------\\
+
+global.tip1 = `Type *${prefa}info* for more information....`
+
+global.tip2 = `Type *${prefa}settings* to commit changes in the bot.`
+
+global.tip3 = `If you got a bug or error, then please report to developer asap by *${prefa}report* command.`
+
+//--------------- Menu images ----------------\\
+
+global.nullImage = fs.readFileSync('./AnyaPikaMedia/theme/Anyatestpic.jpg')
+
+global.nullDoc = fs.readFileSync('./AnyaPikaMedia/theme/storemedia/Anya.xlsx')
+
+global.Menuimage = fs.readFileSync("./AnyaPikaMedia/theme/Menu/Menuimg.jpg") // Thumbnail for Dashboard
+
+global.thumnnaiIs = fs.readFileSync("./AnyaPikaMedia/theme/Menu/Menuimg2.jpeg")
+
+global.allmenuImg = fs.readFileSync('./AnyaPikaMedia/theme/Commandsimgs/Allmenu.jpg') // Thumbnail for Allmenu command 
+
+ 
+
+//--------------- Systemic Function --------------\\
+
+global.autoTyping = false 
+
+global.autoreadpmngc = false 
+
+global.autoreact = true
+
+global.antilink = true
+
+global.autoReadGc = false 
+
+global.autoRecord = false 
+
+global.available = true
+
+global.autoTyping = false 
+
+//----------- Users Userlimit -----------\\
+
+global.userLimit = {
+
+premium: "Infinity",  // premium users 
+
+free: 100000,            // Free users
+
+}
+
+/*𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹 DANGER ZONE ⚠️👇 𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹𝗹*/
+
+let file = require.resolve(__filename)
+
+fs.watchFile(file, () => {
+
+	fs.unwatchFile(file)	console.log(chalk.redBright(`Update'${__filename}'`))
+
+	delete require.cache[file]
+
+	require(file)
+
+})
